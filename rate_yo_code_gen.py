6fdd053f377f6bcc7dea8dc028e3253b95b32e86
Ennima <ennima@hotmail.com>
2017-06-30 02:00:52 -0500

    Cuestionario empty

--- rate_yo_code_gen.py
+++ rate_yo_code_gen.py
@@ -1,5 +1,11 @@
 import os
-
+def es_par(num):
+	valor = False
+	if(num%2 == 0):
+		valor= True
+	if(num%2 != 0):
+		valor = False
+	return valor
 
 text_preguntas =["¿Fue útil el producto o servicio que le ofrecimos?"]
 text_preguntas.append("¿Cómo percibe el profesionalismo de nuestra empresa?")
@@ -14,13 +20,26 @@ num_preguntas = len(text_preguntas)
 def genera_li():
 	for i in range(1,num_preguntas+1):
 		# print(i)
-		genera = '<li>\n	<input type="hidden" name="id" value="'+str(i)+'">\n 	<input type="hidden" name="texto" value="'+text_preguntas[i-1]+'">\n		<div class="texto_div">'+text_preguntas[i-1]+'</div>\n		<div class="rating_div">\n			<div id="rateYo_'+str(i)+'"></div>\n		</div>\n	</div>\n</li>\n\n'
+		class_separa = ""
+		if(es_par(i)):
+			class_separa = ' class="relleno" '
+		else:
+			class_separa = ""
+		genera = '<li'+class_separa+'>\n	<input type="hidden" id="id_'+str(i)+'" name="id_'+str(i)+'" value="'+str(i)+'">\n 	<input type="hidden" id="texto_'+str(i)+'" name="texto_'+str(i)+'" value="'+text_preguntas[i-1]+'">\n		<div class="texto_div">'+text_preguntas[i-1]+'</div>\n		<div class="rating_div">\n			<div id="rateYo_'+str(i)+'"></div>\n		</div>\n	\n</li>\n\n'
 		print(genera)
 
-# genera_li()
+genera_li()
 def genera_rate_js():
 	for i in range(1,num_preguntas+1):
-		genera = '$("#rateYo_'+str(i)+'").rateYo({\n			  	normalFill: "#868686",\n			  	ratedFill: "#0975b2",\n			  	starWidth:"30px",\n			  	// spacing: "5px",\n			    rating: 2,\n			    fullStar: true,\n			    precision: 2\n			  });\n\n'
+		genera = '$("#rateYo_'+str(i)+'").rateYo({\n			  	normalFill: "#868686",\n			  	ratedFill: "#0975b2",\n			  	starWidth:"30px",\n			  	// spacing: "5px",\n			    rating: 1,\n			    fullStar: true,\n			    precision: 2\n			  });\n\n'
 		print(genera)
 
-genera_rate_js()
+#genera_rate_js()
+
+
+def genera_get_rate():
+	for i in range(1, num_preguntas+1):
+		genera = '//Get rating value '+str(i)+'\n	var $rateYo_'+str(i)+' = $("#rateYo_'+str(i)+'").rateYo();\n	var rating_'+str(i)+' = $rateYo_'+str(i)+'.rateYo("rating");\n	console.log("Rate '+str(i)+': "+rating_'+str(i)+');\n\n'
+		print(genera)
+
+# genera_get_rate()
